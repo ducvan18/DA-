3083d04ee416466749a47837130c4c48bc0ba277
ducvan18 <150318633+ducvan18@users.noreply.github.com>
2024-07-30 07:59:00 +0700

    Delete admin/Controllers directory

--- admin/Controllers/CustomerController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Models/Customer.php';
-
-function viewCustomer()
-{
-    $customers = getAllCustomers();
-    include 'Views/Customer/ViewCustomer.php';
-}
--- admin/Controllers/OrderController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Models/Order.php';
-
-function viewOrder()
-{
-    $orders = getAllOrder();
-    include 'Views/Order/ViewOrder.php';
-}
