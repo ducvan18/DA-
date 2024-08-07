2a32bfd6f3ddf181c87b5a38a8b557cf742f2f67
GIANGDEPTRAI88 <giangkoi270604@gmail.com>
2024-08-07 22:07:56 +0700

    Add files via upload

---
+++ env.php
@@ -0,0 +1,7 @@
+<?php
+
+const BASE_URL = "";
+const DBHOST = "localhost";
+const DBNAME = "http://localhost/phpmyadmin/index.php?route=/database/structure&db=nhom10";
+const DBUSER = "root";
+const DBPASS = "";
